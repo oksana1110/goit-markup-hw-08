6678695bd28cd153f2d1db034275f9d7f507e165
oksana1110 <oksana.kysil91@gmail.com>
2023-01-06 02:26:15 +0200

    header+main without graphics

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 (() => {
-  const refs = {
+    const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
