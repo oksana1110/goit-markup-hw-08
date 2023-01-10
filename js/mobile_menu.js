58e3ad6cef80b045e4070c4005538158facaff71
oksana1110 <oksana.kysil91@gmail.com>
2023-01-10 03:53:09 +0200

    mobile menu added

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,4 +1,4 @@
-(() => {
+/*(() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
 
@@ -11,4 +11,18 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+})();*/
+(() => {
+const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    Menu: document.querySelector("[data-menu]"),
+};
+
+refs.openMenuBtn.addEventListener("click", toggleMenu);
+refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    refs.Menu.classList.toggle("is-hidden");
+}
 })();
